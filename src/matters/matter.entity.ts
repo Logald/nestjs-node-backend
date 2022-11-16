f50c32d1f028ec2378b9d3c7e79a392cebb759e3
Sofelixx <s18paris@gmail.com>
2022-11-16 07:05:38 +0000

    the name of entity matter was changed

--- src/matters/matter.entity.ts
+++ src/matters/matter.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('matter')
+@Entity({ name: 'matter' })
 export class Matter {
   @PrimaryGeneratedColumn()
   id: number;
