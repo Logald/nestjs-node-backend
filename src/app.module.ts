f4fdd6b58883b547ef8b4894ef12d3bc01320f5b
Sofelixx <s18paris@gmail.com>
2022-11-16 04:49:57 +0000

    the connection to db was implemented and the .env file was added to the .gitignore file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MattersModule } from './matters/matters.module';
 
 @Module({
-  imports: [MattersModule],
+  imports: [
+    MattersModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env['MYSQL_HOST'] || 'localhost',
+      port: parseInt(process.env['MYSQL_PORT']) || 3306,
+      username: process.env['MYSQL_USER'] || 'root',
+      password: process.env['MYSQL_PASSWORD'] || '',
+      database: process.env['MYSQL_DATABASE'] || 'prueba',
+      entities: [__dirname + '/**/*{.ts,.js}'],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
