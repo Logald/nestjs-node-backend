5a99e008ac5e74dda4b957b4f5513908b847745f
Sofelixx <s18paris@gmail.com>
2022-11-22 05:58:53 +0000

    the get people task was finished

--- src/people/people.controller.ts
+++ src/people/people.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PeopleProvider } from './people.service';
 import { Person } from './person.entity';
 
 @Controller('people')
 export class PeopleController {
   constructor(private peopleProvider: PeopleProvider) {}
+
+  @Get()
+  getPeople() {
+    return this.peopleProvider.getPeople();
+  }
 
   @Post()
   createPerson(@Body() personData: Omit<Person, 'id'>) {
