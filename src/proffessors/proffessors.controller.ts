5da44f4dab5b8b824a80b02cb6c208cc7d0a2c08
Sofelixx <s18paris@gmail.com>
2022-11-24 00:37:01 +0000

    the get proffessors task was finished

--- src/proffessors/proffessors.controller.ts
+++ src/proffessors/proffessors.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Proffessor } from './proffessor.entity';
 import { ProffessorsProvider } from './proffessors.service';
 
 @Controller('proffessors')
 export class ProffessorsController {
   constructor(private proffessorsProvider: ProffessorsProvider) {}
+
+  @Get()
+  getProffessors() {
+    return this.proffessorsProvider.getProffessors();
+  }
 
   @Post()
   createProffessor(@Body() proffessorData: Omit<Proffessor, 'id'>) {
