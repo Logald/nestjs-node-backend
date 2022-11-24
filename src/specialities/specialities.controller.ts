9ea04481897d5192531dfebe3a4e4298609ca3fe
Sofelixx <s18paris@gmail.com>
2022-11-24 05:38:10 +0000

    the get specialities was finished

--- src/specialities/specialities.controller.ts
+++ src/specialities/specialities.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SpecialitiesProvider } from './specialities.service';
 import { Speciality } from './speciality.entity';
 
 @Controller('specialities')
 export class SpecialitiesController {
   constructor(private specialitiesProvider: SpecialitiesProvider) {}
+
+  @Get()
+  getSpecialities() {
+    return this.specialitiesProvider.getSpecialities();
+  }
 
   @Post()
   createSpeciality(@Body() specialityData: Omit<Speciality, 'id'>) {
