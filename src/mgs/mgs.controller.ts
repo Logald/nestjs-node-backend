1494ae6397c51fad8ddcd74ad7ac95e948b3408e
Sofelixx <s18paris@gmail.com>
2022-12-06 05:12:31 +0000

    the get mg task was finished

--- src/mgs/mgs.controller.ts
+++ src/mgs/mgs.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { MG } from './mg.entity';
 import { MGProvider } from './mgs.service';
 
@@ -14,6 +14,11 @@ export class MGController {
   @Get('/all')
   getMgsWithRelations(@Body() mgsFindManyOptions: MG) {
     return this.mgProvider.getMgsWithRelations(mgsFindManyOptions);
+  }
+
+  @Get('/:mgId')
+  getMg(@Param('mgId') mgId: number) {
+    return this.mgProvider.getMg(mgId);
   }
 
   @Post()
